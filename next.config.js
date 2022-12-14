22f724ee21dd55f0098d8e548114aa67449bedf2
Minung Han <hmu332233@gmail.com>
2022-12-15 01:19:32 +0900

    rewrite 옵션 추가

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/badge.svg',
+        destination: '/api/badge.svg',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
